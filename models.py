1c79199e8bf9dee59175578fa2b7beb8f95157d4
JosefAnthony <josefanthony3@gmail.com>
2025-01-14 17:18:22 +0100

    task T381987 review changes

--- models.py
+++ models.py
@@ -5,38 +5,11 @@ db = SQLAlchemy()
 
 # Define a User model
 class User(db.Model):
-    __tablename__ = 'users'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+__tablename__ = 'users'
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+id = db.Column(db.Integer, primary_key=True)
+username = db.Column(db.String(80), unique=True, nullable=False)
+email = db.Column(db.String(120), unique=True, nullable=False)
 
-# Define a Post model
-class Post(db.Model):
-    __tablename__ = 'posts'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Post {self.title}>'
-
-# Define a Comment model
-class Comment(db.Model):
-    __tablename__ = 'comments'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-    user = db.relationship('User', backref='comments')
-    post = db.relationship('Post', backref='comments')
-
-    def __repr__(self):
-        return f'<Comment {self.body[:20]}>'
+def __repr__(self):
+return f'<User {self.username}>'
