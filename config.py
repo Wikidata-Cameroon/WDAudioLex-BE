861f46fdd130c55c706b00f97ba8e30e76a95cfd
Wandji Collins <collinschuwa@gmail.com>
2026-04-02 18:31:52 +0100

    chore: app files and settting

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'WDAudioLex')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # oauth 
+    CONSUMER_KEY = "ef9f619cfa02d15596b61b72ad35ecd4"
+    CONSUMER_SECRET = "e18d71307b49e5235294332ed327563311fd59e2"
+
+    OAUTH_MWURI =  "https://meta.wikimedia.org/w/index.php"
